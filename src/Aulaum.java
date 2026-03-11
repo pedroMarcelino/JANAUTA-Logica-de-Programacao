6a184802f7e948badc97cb97158755ff4ddc52c2
pedroMarcelino <pedro.marcelinodv@gmail.com>
2026-03-11 18:34:55 -0300

    atualizacao aula 1, logica sequencial

--- src/Aulaum.java
+++ src/Aulaum.java
@@ -1,5 +1,12 @@
 public class Aulaum {
     public static void main(String[] args) {
+        System.out.println("logica");
+        System.out.println("sequencial");
+
+        System.out.println("Sequencial");
+        System.out.println("logica");
+        System.out.println("nao está em");
+
 
     }
 }
