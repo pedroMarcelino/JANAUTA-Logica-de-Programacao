9d6aed1282ad08a04ccdfb0f3c012a954864beb0
pedroMarcelino <pedro.marcelinodv@gmail.com>
2026-03-13 16:58:50 -0300

    expressoes comparativas

--- src/entradaSaidaDeDados.java
+++ src/entradaSaidaDeDados.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class entradaSaidaDeDados {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ola digite seu nome: ");
+        String name = scanner.nextLine();
+        System.out.println("nome : " + name);
+
+        System.out.println("Ola digite sua idade : ");
+        int age = scanner.nextInt();
+        System.out.println("Idade : " + age );
+
+        System.out.println("Ola digite seu salario: ");
+        double salary = scanner.nextDouble();
+        System.out.println("salario : " + salary );
+    }
 }
