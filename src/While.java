8a6f71b63c8ef8a8d5e934c81b31822009cb4475
pedroMarcelino <pedro.marcelinodv@gmail.com>
2026-03-13 18:27:48 -0300

    while

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador < 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    }
+}
